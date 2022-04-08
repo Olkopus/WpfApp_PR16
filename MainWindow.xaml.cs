36fd8b4ae3f0f1dc888d931009fce106d857a79a
196390 <196390@KB-120-C06>
2022-04-08 14:50:01 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Threading;
+
+namespace WpfApp_PR16
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded_Date();
+        }
+        private void Loaded_Date()
+        {
+            System.Windows.Threading.DispatcherTimer Timer = new System.Windows.Threading.DispatcherTimer();
+            Timer.Interval = TimeSpan.FromSeconds(0);
+            Timer.Tick += Timer_Tick;
+            Timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            LabelDate.Content = DateTime.Now.ToString();
+        }
+
+        private void Button_ClickBack(object sender, RoutedEventArgs e)
+        {
+            if (MainFrame.CanGoBack) 
+                MainFrame.GoBack();
+        }
+
+        protected override void OnClosing(System.ComponentModel.CancelEventArgs e)
+        {
+            e.Cancel = !(MessageBox.Show("Вы действительно хотите выйти?", "Подтверждение выхода", MessageBoxButton.YesNo, MessageBoxImage.None,MessageBoxResult.No) == MessageBoxResult.Yes);
+
+        }
+
+        private void MainFrame_OnNavigated(object sender, NavigationEventArgs e)
+        {
+            if (!(e.Content is Page page)) return;
+            this.Title = $"ProjectByVoynalovichShergina - {page.Title}";
+
+            if (page is Pages.AuthPage)
+                ButtonBack.Visibility = Visibility.Hidden;
+            else ButtonBack.Visibility = Visibility.Visible;
+
+        }
+    }
+
+
+}
---
+++ Pages/AuthPage.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp_PR16.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthPage.xaml
+    /// </summary>
+    public partial class AuthPage : Page
+    {
+        public AuthPage()
+        {
+            InitializeComponent();
+        }
+        private void TextBoxLogin_Changed(object sender, RoutedEventArgs e)
+        {
+            txtlogin.Visibility = Visibility.Visible;
+            if (TextBoxLogin.Text.Length > 0)
+            {
+                txtlogin.Visibility = Visibility.Hidden;
+            }
+
+        }
+
+ private void PasswordBox_Changed(object sender, RoutedEventArgs e)
+        {
+            txtpassword.Visibility = Visibility.Visible;
+            if (PasswordBox.SecurePassword.Length > 0)
+            {
+                txtpassword.Visibility = Visibility.Hidden;
+            }
+
+        }
+
+        private void Button_Enter(object sender, RoutedEventArgs e)
+        {
+            if(string.IsNullOrEmpty(TextBoxLogin.Text) || string.IsNullOrEmpty(PasswordBox.Password))
+            {
+                MessageBox.Show("Введите логин и пароль!");
+                return;
+            }
+
+            using (var db = new Entities1())
+            {
+                var user = db.Client
+                    .AsNoTracking()
+                    .FirstOrDefault(u => u.Login == TextBoxLogin.Text && u.Password == PasswordBox.Password);
+                if (user == null)
+                {
+                    MessageBox.Show("Пользователь не найден!");
+                }
+                else
+                {
+                    MessageBox.Show("Пользователь успешно найден!");
+                    switch(user.Role)
+                    {
+                        case "Клиент":
+                            NavigationService?.Navigate(new Client());
+                            break;
+                        case "Менеджер":
+                            NavigationService?.Navigate(new Manager());
+                            break;
+
+                    }
+                }
+
+            }
+
+
+        }
+    }
+    
+}
