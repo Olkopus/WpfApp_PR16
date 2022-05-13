217c410b8cc90d81957f1a46b04a37f74cd7adfa
Олеся Войналович <196390@edu.fa.ru>
2022-05-13 15:00:28 +0300

    22 пр сделана

--- Pages/AllSuppliers.xaml.cs
+++ Pages/AllSuppliers.xaml.cs
@@ -23,8 +23,71 @@ namespace WpfApp_PR16.Pages
         public AllSuppliers()
         {
             InitializeComponent();
-            var currentUsers = Entities.GetContext().Supplier.ToList();
-            ListUser.ItemsSource = currentUsers;
+            var currentSupplier = Entities.GetContext().Supplier.ToList();
+            ListUser.ItemsSource = currentSupplier;
+            ComboBoxSort.SelectedIndex = 0;
+            UpdateSuppliers();
+        }
+
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            TextBoxName.Clear();
+      
+            ComboBoxSort.SelectedIndex = 0;
+            TextBoxINN.Clear();
+            UpdateSuppliers();
+        }
+
+        private void UpdateSuppliers()
+        {
+            //загружаем всех пользователей в список
+            var currentsupplier = Entities.GetContext().Supplier.ToList();
+
+           
+            currentsupplier = currentsupplier.Where(x => x.Name.ToLower().Contains(TextBoxName.Text.ToLower())).ToList();
+
+            if(TextBoxINN != null)
+            {
+                currentsupplier = currentsupplier.Where(x => x.INN.ToLower().Contains(TextBoxINN.Text.ToLower())).ToList();
+            }
+           
+          
+            //осуществляем сортировку в зависимости от выбора пользователя
+            if (ListUser != null)
+            {
+                if (ComboBoxSort.SelectedIndex == 0)
+                    ListUser.ItemsSource = currentsupplier.OrderBy(x => x.Name).ToList();
+                else ListUser.ItemsSource = currentsupplier.OrderByDescending(x => x.Name).ToList();
+            }
+
+
+        }
+        
+
+        private void TextBoxName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            txtname.Visibility = Visibility.Visible;
+            if (TextBoxName.Text.Length > 0)
+            {
+                txtname.Visibility = Visibility.Hidden;
+            }
+            UpdateSuppliers();
+        }
+
+        private void TextBoxINN_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            txtinn.Visibility = Visibility.Visible;
+            if (TextBoxINN.Text.Length > 0)
+            {
+                txtinn.Visibility = Visibility.Hidden;
+            }
+            UpdateSuppliers();
+
+        }
+
+        private void ComboBoxSort_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateSuppliers();
         }
     }
 }
--- Pages/AllUsers.xaml.cs
+++ Pages/AllUsers.xaml.cs
@@ -25,7 +25,70 @@ namespace WpfApp_PR16.Pages
             InitializeComponent();
             var currentUsers = Entities.GetContext().User.ToList();
             ListUser.ItemsSource = currentUsers;
+            ComboBoxSort.SelectedIndex = 0;
+            CheckBoxOnlyClients.IsChecked = false;
+
+            UpdateUsers();
+
+        }
+
+        private void UpdateUsers()
+        {
+           
+            //загружаем всех пользователей в список
+            var currentUsers = Entities.GetContext().User.ToList();
+
+            //осуществляем поиск по Ф.И.О. без учета регистра букв
             
+   
+            currentUsers = currentUsers.Where(x => x.Surname.ToLower().Contains(TextBoxFIO.Text.ToLower())).ToList();
+
+
+            //обрабатываем фильтр по одной роли пользователей
+            if (CheckBoxOnlyClients !=null && CheckBoxOnlyClients.IsChecked.Value)
+                currentUsers = currentUsers.Where(x => x.Role.Contains("Клиент")).ToList();
+
+            //осуществляем сортировку в зависимости от выбора пользователя
+            if (ListUser != null)
+            {
+                if(ComboBoxSort.SelectedIndex == 0)
+                    ListUser.ItemsSource = currentUsers.OrderBy(x => x.Surname).ToList();
+                else ListUser.ItemsSource = currentUsers.OrderByDescending(x => x.Surname).ToList();
+            }
+
+        }
+
+            private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            txtfio.Visibility = Visibility.Visible;
+            if (TextBoxFIO.Text.Length > 0)
+            {
+                txtfio.Visibility = Visibility.Hidden;
+            }
+            UpdateUsers();
+        }
+
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            TextBoxFIO.Clear();
+            CheckBoxOnlyClients.IsChecked = false;
+            ComboBoxSort.SelectedIndex = 0;
+            UpdateUsers();
+        }
+
+        private void ComboBoxSort_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateUsers();
+        }
+
+        private void CheckBoxOnlyClients_Checked(object sender, RoutedEventArgs e)
+        {
+            UpdateUsers();            
+        }
+
+        private void CheckBoxOnlyClients_Unchecked(object sender, RoutedEventArgs e)
+        {
+            UpdateUsers();         
         }
     }
 }
